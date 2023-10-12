e82dd88c787d84a6c97a5987d5c633cafd24b3e6
flushtron9 <suresh.kada@gmail.com>
2023-10-11 22:28:12 -0400

    Structure

---
+++ api/src/main/java/com/wimthackathon/codegptuiapi/CodegptuiApiApplication.java
@@ -0,0 +1,13 @@
+package com.wimthackathon.codegptuiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegptuiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodegptuiApiApplication.class, args);
+	}
+
+}
